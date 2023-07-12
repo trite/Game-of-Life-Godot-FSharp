7533dc43702ece327e15b78e720f4a2d56019ec0
trite <trite@users.noreply.github.com>
2023-07-12 12:40:02 -0700

    checkin before clearing out previous .godot folder stuff

--- FsLib/World.fs
+++ FsLib/World.fs
@@ -1,2 +1,10 @@
-﻿module World
+﻿namespace FsLib
 
+open Godot
+
+
+
+module World =
+    let cell: Sprite2D option = None
+
+    let _ready: unit -> unit = fun () -> GD.Print("Hello from FSharp!!!!!")
--- World.cs
+++ World.cs
@@ -4,77 +4,79 @@ using System.Linq;
 
 public partial class World : Node2D
 {
-	private Sprite2D? cell = null;
-	private Color aliveColor;
-	private Godot.Collections.Dictionary cells = new Godot.Collections.Dictionary();
+  private Sprite2D? cell = null;
+  private Color aliveColor;
+  private Godot.Collections.Dictionary cells = new Godot.Collections.Dictionary();
 
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
+  // Called when the node enters the scene tree for the first time.
+  public override void _Ready()
+  {
+	FsLib.World._ready();
+	// GD.Print(FsLib.Say.hello("World"));
+
+	// cell = GetNode<Sprite2D>("Cell");
+
+	// if (cell != null)
+	// {
+	// 	cell.Hide();
+	// 	aliveColor = cell.Modulate;
+	// }
+	// else
+	// {
+	// 	throw new NullReferenceException("$Cell shouldn't be null!");
+	// }
+  }
+
+  // Called every frame. 'delta' is the elapsed time since the previous frame.
+  public override void _Process(double delta)
+  {
+  }
+
+  public override void _UnhandledInput(InputEvent @event)
+  {
+	if (@event is InputEventMouseButton mouseButton)
 	{
-		GD.Print(FsLib.Say.hello("World"));
+	  if (mouseButton.ButtonIndex == MouseButton.Left && mouseButton.Pressed)
+	  {
 
-		cell = GetNode<Sprite2D>("Cell");
+	  }
+	}
+  }
 
-		if (cell != null)
-		{
-			cell.Hide();
-			aliveColor = cell.Modulate;
-		}
-		else
-		{
-			throw new NullReferenceException("$Cell shouldn't be null!");
-		}
+  public void PlaceCell(Vector2 pos)
+  {
+	var gridPos = GetGridPosition(pos);
+	// if (!)
+
+  }
+
+  public Vector2 GetGridPosition(Vector2 pos)
+  {
+	float pixels = 32.0F;
+	return pos.Snapped(new Vector2(pixels, pixels)) / pixels;
+  }
+
+  public void AddNewCell(Vector2 gridPosition)
+  {
+	var pos = gridPosition * 32.0F;
+	var newCell = cell?.Duplicate() as Sprite2D;
+
+	if (newCell is null)
+	{
+	  throw new Exception("Cell is null!");
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+	newCell.Position = pos;
+	AddChild(newCell);
+	newCell.Show();
+
+	if (cells.ContainsKey(gridPosition))
 	{
+	  throw new Exception("Cell already exists!");
 	}
-
-	public override void _UnhandledInput(InputEvent @event)
+	else
 	{
-		if (@event is InputEventMouseButton mouseButton)
-		{
-			if (mouseButton.ButtonIndex == MouseButton.Left && mouseButton.Pressed)
-			{
-
-			}
-		}
+	  cells.Add(gridPosition, newCell);
 	}
-
-	public void PlaceCell(Vector2 pos)
-	{
-		var gridPos = GetGridPosition(pos);
-		if (!)
-	}
-
-	public Vector2 GetGridPosition(Vector2 pos)
-	{
-		float pixels = 32.0F;
-		return pos.Snapped(new Vector2(pixels, pixels)) / pixels;
-	}
-
-	public void AddNewCell(Vector2 gridPosition)
-	{
-		var pos = gridPosition * 32.0F;
-		var newCell = cell?.Duplicate() as Sprite2D;
-		
-		if (newCell is null)
-		{
-			throw new Exception("Cell is null!");
-		}
-		
-		newCell.Position = pos;
-		AddChild(newCell);
-		newCell.Show();
-
-		if (cells.ContainsKey(gridPosition))
-		{
-			throw new Exception("Cell already exists!");
-		}
-		else
-		{
-			cells.Add(gridPosition, newCell);
-		}	
-	}
+  }
 }
